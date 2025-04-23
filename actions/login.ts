cbcdc8eebd07780dedce7f856284f66cc621716c
InGwonKang <tnqkr3494@naver.com>
2025-04-23 21:18:56 +0900

    feat: resend 적용 및 토큰 검증 이메일 발송 로직 작성

--- actions/login.ts
+++ actions/login.ts
@@ -6,6 +6,9 @@ import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
+import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFileds = LoginSchema.safeParse(values);
@@ -15,6 +18,26 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   }
 
   const { email, password } = validatedFileds.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: "Email doesn't exist" };
+  }
+
+  // 이 부분은 auth.ts callback에서 추가로 막아준다.
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(
+      existingUser.email
+    );
+
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token
+    );
+
+    return { success: "Confirmation email sent!" };
+  }
 
   try {
     await signIn("credentials", {
--- auth.ts
+++ auth.ts
@@ -26,6 +26,20 @@ export const {
     },
   },
   callbacks: {
+    async signIn({ user, account }) {
+      // Allow oAUth without email verification
+      if (account?.provider !== "credentials") return true;
+
+      const existingUser = await getUserById(user.id!);
+
+      // Prevent sign in without verification
+      if (!existingUser?.emailVerified) return false;
+
+      // TODO: Add 2FA check
+
+      return true;
+    },
+
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;

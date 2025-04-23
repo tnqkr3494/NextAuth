58c4da979d7920c922427ecb201ca306097b5dd0
InGwonKang <tnqkr3494@naver.com>
2025-04-23 21:51:41 +0900

    feat: 발송된 토큰 검증로직 작성

---
+++ actions/new-verification.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
+import { getVerificationTokenByToken } from "@/data/verification-token";
+
+export const newVerification = async (token: string) => {
+  const existingToken = await getVerificationTokenByToken(token);
+
+  if (!existingToken) {
+    return { error: "Token does not exist!" };
+  }
+
+  const hasExpired = new Date(existingToken.expires) < new Date();
+
+  if (hasExpired) {
+    return { error: "Token has expired!" };
+  }
+
+  const existingUser = await getUserByEmail(existingToken.email);
+
+  if (!existingUser) {
+    return { error: "Email does not exist!" };
+  }
+
+  await db.user.update({
+    where: {
+      id: existingUser.id,
+    },
+    data: {
+      emailVerified: new Date(),
+      // 추후 사용자가 이메일 변경할 때 재사용하기 위해 아래 코드 사용
+      email: existingToken.email,
+    },
+  });
+
+  await db.verificationToken.delete({
+    where: { id: existingToken.id },
+  });
+
+  return { success: "Email verified" };
+};

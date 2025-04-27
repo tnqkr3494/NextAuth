84afbe25a3fa277d9c3fa1c0ce7635655bded794
InGwonKang <tnqkr3494@naver.com>
2025-04-27 23:31:11 +0900

    feat: setting 페이지 UI 추가

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,20 +1,21 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-const Settings = async () => {
-  const session = await auth();
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+const Settings = () => {
+  // client 컴포넌트에서 session을 가져오고 싶을 때
+  const user = useCurrentUser();
+
+  const onClick = () => {
+    logout();
+  };
 
   return (
-    <div>
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          "use server";
-
-          await signOut();
-        }}
-      >
-        <button type="submit">Sign out</button>
-      </form>
+    <div className="bg-white p-10 rounded-xl">
+      <button type="submit" onClick={onClick}>
+        Sign out
+      </button>
     </div>
   );
 };
---
+++ components/auth/user-button.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { ExitIcon } from "@radix-ui/react-icons";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuContent,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LogoutButton } from "./logout-button";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+export const UserButton = () => {
+  const user = useCurrentUser();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={user?.image || "https://github.com/shadcn.png"} />
+          <AvatarFallback>SC</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-40" align="end">
+        <LogoutButton>
+          <DropdownMenuItem>
+            <ExitIcon className="w-4 h-4 mr-2" />
+            Logout
+          </DropdownMenuItem>
+        </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
